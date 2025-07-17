039765e9f7ce15b8348f4e8ef5be14ced83444d0
Charalambos Emmanouilidis <charalambos.emmanouilidis@gmail.com>
2025-07-17 21:18:02 +0200

    Update feature vscode-auto-extension

--- src/vscode-auto-extensions/install.sh
+++ src/vscode-auto-extensions/install.sh
@@ -5,27 +5,25 @@ echo "[vscode-auto-extensions] Installing dependencies..."
 apt-get update && apt-get install -y jq curl
 
 echo "[vscode-auto-extensions] Installing extensions..."
+# workarround to support installing extension in coder.com workspaces
+# https://github.com/coder/envbuilder/issues/43
 
-MARKER="/home/vscode/.vscode-extensions-installed"
-
-if [ ! -f "$MARKER" ]; then
-  echo "[vscode-auto-extensions] VSCode detected. Installing extensions..."
-
-  CODE_CLI=code
-  if command -v code-server &>/dev/null; then
-    CODE_CLI=code-server
-  fi
-
-  mkdir -p ~/.vscode-server/extensions
-
-  extensions=$(sed 's/\/\/.*$//g' .devcontainer/devcontainer.json | jq -r -M '[.customizations.vscode.extensions[]?, .extensions[]?] | .[]')
-
-  for ext in $extensions; do
-    $CODE_CLI --extensions-dir ~/.vscode-server/extensions --install-extension "$ext"
-  done
-
-  touch "$MARKER"
-else
-  echo "[vscode-auto-extensions] Skipped extension install: not VSCode or already installed."
+if ! which code-server > /dev/null; then
+  curl -fsSL https://code-server.dev/install.sh | sh | tee code-server-install.log
 fi
 
+CODE_CLI=code
+if code-server -v > /dev/null; then
+  CODE_CLI=code-server
+fi
+
+mkdir -p ~/.vscode-server/extensions
+
+set +e
+extensions=( $(sed 's/\/\/.*$//g' .devcontainer/devcontainer.json | jq -r -M '[.customizations.vscode.extensions[]?, .extensions[]?] | .[]' ) )
+
+if [ "${extensions[0]}" != "" ] && [ "${extensions[0]}" != "null" ]; then
+  for extension in "${extensions[@]}"; do
+    $CODE_CLI --extensions-dir ~/.vscode-server/extensions --install-extension "$extension"
+  done
+fi
